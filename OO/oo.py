8e2d9d9ff14f65ef9fbbd0a3f834e234738097ee
Hugo Oliveira <hugo684@gmail.com>
2021-07-12 17:02:47 -0300

    added withdraw method

--- OO/oo.py
+++ OO/oo.py
@@ -7,13 +7,28 @@ class BankAccount():
         self.owner = owner
         self.current_balance = Decimal(current_balance)
 
-    def balance_operation(self, added_value):
-        self.current_balance += Decimal(added_value)
+    def deposit(self, added_value):
+        if added_value > 0:
+            self.current_balance += Decimal(added_value)
+        else:
+            raise ValueError(
+                'An invalid ammount was given for the operation. Deposit values should be greater than zero')
+
+    def withdraw(self, withdrawn_value):
+        if withdrawn_value <= 0:
+            raise ValueError(
+                'An invalid ammount was given for the operation. Deposit values should be greater than zero')
+
+        if withdrawn_value <= self.current_balance:
+            self.current_balance -= Decimal(withdrawn_value)
+
+        else:
+            raise ValueError('Withdraw ammount greater than account balance.')
 
 
 account1 = BankAccount('123-4', 'Hugo', 1000)
 
 
-account1.balance_operation(-300)
+account1.deposit(300)
 
 print(account1.current_balance)
