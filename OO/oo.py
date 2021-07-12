ebe0339e872717ebb65c4d3ab03854e9ee2a165a
Hugo Oliveira <hugo684@gmail.com>
2021-07-12 16:41:22 -0300

    corrected decimal type to prevent bugs stemming from using floating point numbers

--- OO/oo.py
+++ OO/oo.py
@@ -1,15 +1,19 @@
-from decimal import Decimal, getcontext
-
-getcontext().prec = 2
+from decimal import Decimal
 
 
 class BankAccount():
     def __init__(self, id: str, owner: str, current_balance: Decimal) -> None:
         self.id = id
         self.owner = owner
-        self.current_balance = current_balance
+        self.current_balance = Decimal(current_balance)
+
+    def balance_operation(self, added_value):
+        self.current_balance += Decimal(added_value)
 
 
-account1 = BankAccount('123-4', 'Hugo', (Decimal(0.1) + Decimal(0.2)))
+account1 = BankAccount('123-4', 'Hugo', 1000)
+
+
+account1.balance_operation(-300)
 
 print(account1.current_balance)
