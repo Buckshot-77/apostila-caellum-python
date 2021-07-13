ec1cf5dd606492d0cd17e7cba71725f699c8c303
Hugo Oliveira <hugo684@gmail.com>
2021-07-13 15:56:16 -0300

    added static methods

--- OO/classes.py
+++ OO/classes.py
@@ -11,16 +11,39 @@ class Client():
         self.client_since = datetime.today()
 
 
+class History():
+    def __init__(self) -> None:
+        self.creation_date = datetime.today()
+        self.transactions = []
+
+    def get_history(self):
+        return self.transactions
+
+
 class BankAccount():
+    _total_accounts = 0
+
+    @staticmethod
+    def get_total_accounts():
+        return BankAccount._total_accounts
+
     def __init__(self, id: str, owner: Client, current_balance: Decimal, credit_card_limit: Decimal = 1500) -> None:
         self.id = id
         self.owner = owner
         self.current_balance = Decimal(current_balance)
         self.credit_card_limit = Decimal(credit_card_limit)
+        self.history = History()
+        BankAccount._total_accounts += 1
 
     def deposit(self, added_value):
         if added_value > 0:
-            self.current_balance += Decimal(added_value)
+            self.current_balance += added_value
+            self.history.transactions.append({
+                'action': 'deposit',
+                'ammount': added_value,
+                'type': type(self.current_balance)
+            })
+            return True
         else:
             raise ValueError(
                 'An invalid ammount was given for the operation. Deposit values should be greater than zero')
@@ -31,7 +54,11 @@ class BankAccount():
                 'An invalid ammount was given for the operation. Deposit values should be greater than zero')
 
         if withdrawn_value <= self.current_balance:
-            self.current_balance -= Decimal(withdrawn_value)
+            self.current_balance -= withdrawn_value
+            self.history.transactions.append({
+                'action': 'withdraw',
+                'ammount': withdrawn_value
+            })
             return True
 
         else:
--- OO/instances.py
+++ OO/instances.py
@@ -4,4 +4,9 @@ client1 = Client('Hugo', 'Rua A', '00000000000', '01/01/1900')
 
 account1 = BankAccount('3205-8', client1, 1000)
 
-print(account1.owner.name)
+account1.withdraw(300)
+account1.deposit(1500)
+
+account2 = BankAccount('32156', client1, 2000)
+
+print(BankAccount.get_total_accounts())
